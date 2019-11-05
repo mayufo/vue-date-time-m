3380f124e4f989e0d50b0b5fcdcb65516b9770d6
liuhuimin <mayufo@aliyun.com>
2019-11-05 17:29:32 +0800

    新版本准备

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './assets/css/fonts/iconfont.css'; // 引入字体图标样式
+import './assets/css/reset.css'; // 引入字体图标样式
 new Vue({
   render: h => h(App),
 }).$mount('#app')
