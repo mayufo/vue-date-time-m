73518329567fb40449257e03f4272babd26c9ac0
liuhuimin <mayufo@aliyun.com>
2019-11-05 16:07:01 +0800

    新版本准备

--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import VueHighlightJS from 'vue-highlightjs'
-
-
-/*
- * Use Vue Highlight.js
- */
-Vue.use(VueHighlightJS);
-
-import VueDateTimeM from './dateTime/dDateTime.vue'
-Vue.component('date-time', VueDateTimeM);
-
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.config.ignoredElements = [
-  'date-time'
-];
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+}).$mount('#app')
